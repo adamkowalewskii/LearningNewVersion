771d6e0ce8ca17fab9d71dd49b09bb2bc72fa116
Adam Kowalewski <akowalewski@deloittece.com>
2022-12-05 14:46:46 +0100

    Init Commit

---
+++ src/main/java/com/example/newSpringVersionProject/NewSpringVersionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.newSpringVersionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringVersionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringVersionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newSpringVersionProject/NewSpringVersionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newSpringVersionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringVersionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
